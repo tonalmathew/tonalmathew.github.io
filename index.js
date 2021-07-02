653ce6cfde71ed204b6e47e2f730b789e57e25ec
tonalmathew <tonalmathew1999@gmail.com>
2021-07-02 22:36:56 +0530

    update styling

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ $('.nav-link').click(function(){
   $('.nav-link').removeClass('active');
   $(this).addClass('active');
 })
+
+
+$('.navbar-brand').click(function(){
+  $('.nav-link').removeClass('active');
+  $('.home').addClass('active');
+})
+
+$('.about-me').click(function(){
+  $('.nav-link').removeClass('active');
+  $('.about').addClass('active');
+})
