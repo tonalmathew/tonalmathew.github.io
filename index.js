a75a9955d44bdfebd98f538d88a3895abe25d99f
tonalmathew <tonalmathew1999@gmail.com>
2021-07-03 14:17:55 +0530

    feat: scrollspy

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-$('.nav-link').click(function(){
-  $('.nav-link').removeClass('active');
-  $(this).addClass('active');
-})
+// $('.nav-link').click(function(){
+//   $('.nav-link').removeClass('active');
+//   $(this).addClass('active');
+// })
 
 
-$('.navbar-brand').click(function(){
-  $('.nav-link').removeClass('active');
-  $('.home').addClass('active');
-})
+// $('.navbar-brand').click(function(){
+//   $('.nav-link').removeClass('active');
+//   $('.home').addClass('active');
+// })
 
-$('.about-me').click(function(){
-  $('.nav-link').removeClass('active');
-  $('.about').addClass('active');
-})
+// $('.about-me').click(function(){
+//   $('.nav-link').removeClass('active');
+//   $('.about').addClass('active');
+// })
